aeee9254fdf7ba8e1a46142250020ff64e940d47
Damon Collins <damon.c.p.collins@gmail.com>
2019-01-15 13:24:50 -0500

    done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//Your code here
+//Your code h
+function justInvoke(fn){
+  return fn()
+}
+
+function setThisWithCall(fn, n, arg){
+  return fn.call(n, arg)
+}
+
+function setThisWithApply(fn, n, arg){
+  return fn.apply(n, arg)
+}
+
+function returnNewFunctionOf(fn, arg){
+  return fn.bind(arg)
+}
